98544dbc4087f4c3f8ca435fec20c4f370fc607a
Jefferson Silva <jeffsilva01.dev@gmail.com>
2024-10-15 22:27:37 -0300

    layout mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { sans: ["Inter", "sans-serif"] },
+      colors: {
+        "viva-orange": "#F0401E",
+        "viva-blue": "#0470AA",
+      },
+      backgroundImage: {
+        "viva-gradient":
+          "linear-gradient(106deg, rgba(166,32,6,1) 0%, rgba(240,64,30,1) 35%, rgba(255,137,102,1) 100%)",
+      },
+    },
   },
   plugins: [],
 };
